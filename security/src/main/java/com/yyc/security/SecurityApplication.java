2979ed3086917d063e6b923bcb398ee5c60fe74f
gitmuyi <873879043@qq.com>
2018-07-22 22:41:16 +0800

    fix

--- security/src/main/java/com/yyc/security/SecurityApplication.java
+++ security/src/main/java/com/yyc/security/SecurityApplication.java
@@ -7,7 +7,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 @MapperScan("com.yyc.security.dao")
 public class SecurityApplication {
-
     public static void main(String[] args) {
         SpringApplication.run(SecurityApplication.class, args);
     }
