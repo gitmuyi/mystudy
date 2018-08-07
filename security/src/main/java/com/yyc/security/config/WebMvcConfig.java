6ba269780ac4af8b36f277d717b4b9df336fb794
gitmuyi <873879043@qq.com>
2018-08-07 10:22:12 +0800

    kk

--- security/src/main/java/com/yyc/security/config/WebMvcConfig.java
+++ security/src/main/java/com/yyc/security/config/WebMvcConfig.java
@@ -2,6 +2,7 @@ package com.yyc.security.config;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
@@ -17,5 +18,13 @@ public class WebMvcConfig implements WebMvcConfigurer {
         registry.addViewController("/login").setViewName("login");
     }
 
-
+    /**
+     * 放过静态资源
+     * @param registry
+     */
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        System.out.println("ssssssssssssss");
+        registry.addResourceHandler("/static/js/*.js").addResourceLocations().resourceChain(true);
+    }
 }
