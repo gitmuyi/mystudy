587122c0bba1b71e46b3b220568208eba798668d
gitmuyi <873879043@qq.com>
2018-09-26 16:09:27 +0800

    测试环境

--- websocket-demo/src/test/java/com/yyc/websocketdemo/WebsocketDemoApplicationTests.java
+++ websocket-demo/src/test/java/com/yyc/websocketdemo/WebsocketDemoApplicationTests.java
@@ -4,6 +4,7 @@ import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.context.annotation.Bean;
 import org.springframework.test.context.junit4.SpringRunner;
@@ -12,11 +13,12 @@ import org.springframework.web.socket.server.standard.ServerEndpointExporter;
 import java.io.IOException;
 
 @RunWith(SpringRunner.class)
-@SpringBootTest
+//@SpringBootTest
+@WebMvcTest
 public class WebsocketDemoApplicationTests {
     @Test
     public void contextLoads() throws IOException {
+        System.out.println("sdf");
 
-        WebSocketServer.sendInfo("hah");
     }
 }
